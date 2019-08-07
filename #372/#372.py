91376dea8122e02826125797d21505dbc23d6dfd
Derrick <freemandc@vcu.edu>
2019-08-07 16:41:21 -0500

    Initial commit

---
+++ #372/#372.py
@@ -0,0 +1,28 @@
+"""
+s = list(input("Enter x's and y's: "))
+
+if s.count("x") == s.count("y"):
+    print("True")
+else:
+    print("False")
+"""
+
+# Bonus
+
+from collections import Counter
+
+s = str(input("Enter x's and y's: "))
+counts = Counter(s)
+
+first_value = []
+for i, j in enumerate(counts.values()):
+    if i == 0:
+        first_value.append(j)
+    else:
+        if j == first_value[0]:
+            continue
+        else:
+            print("False")
+            exit()
+
+print("True")
