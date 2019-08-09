706c496a9ffa99b03df57635ab663b41a59a6b20
Derrick <freemandc@vcu.edu>
2019-08-09 10:27:08 -0500

    Completed challenge 380

---
+++ #380/#380.py
@@ -0,0 +1,40 @@
+morse_code_dict = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+}
+
+smorse = str(input("Enter text to convert to morse code: ")).upper()
+
+morse_code = ""
+for letter in smorse:
+    if letter in morse_code_dict:
+        value = morse_code_dict[letter]
+        morse_code += value
+    else:
+        print("Make sure the text you enter is only letters.")
+
+print(morse_code)
